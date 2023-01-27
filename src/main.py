573c2d313e50c1dcea529296d13302ae273733e3
FZapata <97852383+felipezapata01@users.noreply.github.com>
2023-01-27 14:49:32 -0500

    Call OpenAI for text responses

--- src/main.py
+++ src/main.py
@@ -1,36 +1,64 @@
+from dotenv import load_dotenv
+import os
 import speech_recognition as sr
+import openai
+
+load_dotenv()
+
+openai.api_key = os.getenv('OPENAI_KEY')
 
 recognizer = sr.Recognizer()
 mic = sr.Microphone()
-    
+
+
 def recognize_using_google(audio) -> dict:
-    
+
     response = {
-            "success": True,
-            "error": None,
-            "transcription": None
-        }
+        "success": True,
+        "error": None,
+        "transcription": None
+    }
     try:
+        print('Calling google to recognize audio')
         response['transcription'] = recognizer.recognize_google(audio)
     except sr.RequestError:
         response["success"] = False
         response["error"] = "API unavailable"
     except sr.UnknownValueError:
         response["error"] = "Unable to recognize speech"
-    
+
     return response
 
-def main() -> None:
 
+def listen():
     with mic as source:
         print('listening...')
         audio = recognizer.listen(source)
-        print('Completed!!')
-    
-    print('Calling whisper to recognize audio')
-    recognize_using_google(audio)
+        print('Done!!')
+    return audio
+
+
+def forward_to_openai_chat(prompt):
+    model_engine = "text-davinci-003"
+    completion = openai.Completion.create(
+        engine=model_engine,
+        prompt=prompt,
+        max_tokens=1024,
+        n=1,
+        stop=None,
+        temperature=0.5,
+    )
+    response = completion.choices[0].text
+    return response
+
+
+def main() -> None:
+    audio = listen()
+    text = recognize_using_google(audio)['transcription']
+    print('generated prompt: ', text)
+    response = forward_to_openai_chat(text)
+    print(f'Response from OpenAI:\n """{response}\n"""')
 
 
 if __name__ == '__main__':
     main()
-    
